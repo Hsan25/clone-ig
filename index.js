ecfcd9845b5e14d6907babc129faec1ae4414002
Your Name <you@example.com>
2023-06-04 12:40:26 +0700

    update

--- index.js
+++ index.js
@@ -8,9 +8,47 @@ logo.addEventListener("click", (e) => {
     
 })
 
+// notif toggle
+
+const notifBtn = document.querySelector(".show-notif");
+const boxNotif = document.querySelector(".notif");
+const closeNotif = document.querySelector(".close-notif")
+// content toggle 
+const content = document.querySelector(".container");
+
+// nav top toggle
+const navTop = document.querySelector(".nav-top")
+const sliderStatus = document.querySelector(".slider-status")
+notifBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    boxNotif.classList.toggle("active");
+    content.classList.toggle("none");
+    navTop.classList.toggle("none");
+    sliderStatus.classList.toggle("none");
+
+})
+closeNotif.addEventListener("click", (e) => {
+    e.preventDefault();
+    boxNotif.classList.remove("active");
+    navTop.classList.remove("none");
+    content.classList.remove("none");
+    sliderStatus.classList.remove("none");
+})
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener("click", (e) => {
     if(!logo.contains(e.target) && !boxLogo.contains(e.target)){
         boxLogo.classList.remove("active")
 
     }
 })
+
