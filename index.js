1795ae43009fa0063db827a9dadd64ea69f9208c
Your Name <you@example.com>
2023-06-05 21:03:22 +0700

    update

--- index.js
+++ index.js
@@ -1,96 +1,106 @@
-
 // logo toggle
 const logo = document.querySelector(".logo");
 const boxLogo = document.querySelector(".logo-click");
 logo.addEventListener("click", (e) => {
-    e.preventDefault();
-    boxLogo.classList.toggle("active")
-    
-})
+  e.preventDefault();
+  boxLogo.classList.toggle("active");
+});
 
 // notif toggle
 
 const notifBtn = document.querySelector(".show-notif");
 const boxNotif = document.querySelector(".notif");
-const closeNotif = document.querySelector(".close-notif")
-// content toggle 
+const closeNotif = document.querySelector(".close-notif");
+// content toggle
 const content = document.querySelector(".container");
 
 // nav top toggle
-const navTop = document.querySelector(".nav-top")
-const sliderStatus = document.querySelector(".slider-status")
+const navTop = document.querySelector(".nav-top");
+const sliderStatus = document.querySelector(".slider-status");
 notifBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    boxNotif.classList.toggle("active");
-    setTimeout(() =>{
-        content.classList.toggle("none");
-        navTop.classList.toggle("none");
-        sliderStatus.classList.toggle("none");
-
-    },150)
-
-})
+  e.preventDefault();
+  boxNotif.classList.toggle("active");
+  setTimeout(() => {
+    content.classList.toggle("none");
+    navTop.classList.toggle("none");
+    sliderStatus.classList.toggle("none");
+  }, 150);
+});
 closeNotif.addEventListener("click", (e) => {
-    e.preventDefault();
-    boxNotif.classList.remove("active");
-    navTop.classList.remove("none");
-    content.classList.remove("none");
-    sliderStatus.classList.remove("none");
-})
+  e.preventDefault();
+  boxNotif.classList.remove("active");
+  navTop.classList.remove("none");
+  content.classList.remove("none");
+  sliderStatus.classList.remove("none");
+});
 
 // plus toggle
 
 const plus = document.querySelector(".show-plus");
 const boxPlus = document.querySelector(".plus-click");
 plus.addEventListener("click", (e) => {
-    e.preventDefault();
-    boxPlus.classList.toggle("active");
-})
+  e.preventDefault();
+  boxPlus.classList.toggle("active");
+});
 
 // boxHmaburger toggle
 const hamburger = document.querySelectorAll(".hamburger");
 const boxHamburger = document.querySelector(".box-hamburger");
 const closeHamburger = document.querySelectorAll(".close-hamburger");
-hamburger.forEach((a) => {
-    a.addEventListener("click", (e) => {
-        e.preventDefault();
-        boxHamburger.classList.toggle("active");
-    })
-})
+
+
+
+
+// buat variable untuk menampung nilai identitas element
+let x = null
+
+hamburger.forEach((a, i) => {
+  // buat identitas setiap element
+  a.setAttribute("data-index", i);
+  
+  a.addEventListener("click", function (e) {
+    e.preventDefault();
+    boxHamburger.classList.toggle("active");
+
+    // masukkan identitas element yang kita click  
+    x = this.getAttribute("data-index");
+  });
+});
+
 closeHamburger.forEach((a) => {
-    a.addEventListener("click", (e) => {
-        e.preventDefault();
-        boxHamburger.classList.remove("active");
-    })
-})
-
-// hamburger.forEach((a) => {
-//     document.onclick = (e)=>{
-//         if(!a.contains(e.target) && !boxHamburger.contains(e.target)){
-//             boxHamburger.classList.remove("active");
-//             console.log(e.target)
-//         }
-//     }
-// });
-
-
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    boxHamburger.classList.remove("active");
+  });
+});
 
 
 
 
 
 document.addEventListener("click", (e) => {
-    if(!logo.contains(e.target) && !boxLogo.contains(e.target)){
-        boxLogo.classList.remove("active")
-    }
+  // close boxLogo
+  if (!logo.contains(e.target) && !boxLogo.contains(e.target)) {
+    boxLogo.classList.remove("active");
+    console.log("logo");
+  }
+
+  // close boxPlus
+  if (!plus.contains(e.target) && !boxPlus.contains(e.target)) {
+    boxPlus.classList.remove("active");
+  }
 
 
-    if(!plus.contains(e.target) && !boxPlus.contains(e.target)){
-        boxPlus.classList.remove("active")
-    }
+  // close boxHamburger
+    (function closeH(){
+      if(!x){
+        return
+      }
+      if(!hamburger[x].contains(e.target) && !boxHamburger.contains(e.target)){
+        boxHamburger.classList.remove("active");
+      }
+    })();
 
-   
-    
-    
-})
 
+
+});
