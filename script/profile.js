c655905a5552dbe3c4d7ce368d9f15fcec90919b
Your Name <you@example.com>
2023-06-23 22:26:47 +0700

    update

--- script/profile.js
+++ script/profile.js
@@ -40,3 +40,62 @@ document.addEventListener("click", (e) => {
         boxMenu.classList.remove("active");
     }
 })
+
+
+// show edit profile toggle
+
+const btnShowProfile = document.querySelector(".show-edit-profile");
+const boxEditProfile = document.querySelector(".form-edit-profile")
+const closeEditProfile = document.querySelector(".close-profile");
+const btnSubmitProfile = document.querySelector(".submit-profile");
+const x =   document.body.children
+
+function none(){
+    for (i = 0 ; i < x.length;i++){
+        if(x[i].className == "form-edit-profile active"){
+            console.log("lewat");
+            continue
+        }
+        x[i].classList.add("none");
+        
+    }
+}
+function remove(){
+    for (i = 0 ; i < x.length;i++){
+        x[i].classList.remove("none");
+    }
+}
+
+btnShowProfile.addEventListener("click", function(e) {
+    boxEditProfile.classList.add("active");
+    none()
+    console.log("heloo")
+});
+
+
+closeEditProfile.addEventListener("click", (e) => {
+    e.preventDefault()
+    boxEditProfile.classList.remove("active");
+    remove()
+})
+
+
+
+// submit data
+const btnSumbitProfile = document.querySelector(".submit-profile");
+const nameUser = document.querySelector("#nama-user");
+const namaPengguna = document.querySelector("#nama-pengguna");
+const bioUser = document.querySelector("#bio");
+const kataGantiUser = document.querySelector("#kata-ganti");
+const jenisKelaminUser = document.querySelector("#jenis-kelamin");
+// 
+
+const username = document.querySelector(".username");
+const namePengguna = document.querySelector(".name-pengguna");
+
+btnSumbitProfile.addEventListener("click", (e) => {
+    username.value = nameUser.value
+    namePengguna.value = namaPengguna.value
+    console.log(username.value)
+    remove()
+})
