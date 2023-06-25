4659075b3ac3ff9b10dbc2ccd8828a1d7ced7ff6
Your Name <you@example.com>
2023-06-25 20:57:33 +0700

    update

--- script/chat.js
+++ script/chat.js
@@ -1,9 +1,10 @@
 // toggle focus
 
 const message = document.querySelector(".message");
+const messageText = message.value;
 const icons = document.querySelector(".icons");
 const btnSend = document.querySelector(".send");
-const contentRight = document.querySelector(".right");
+const contentMain = document.querySelector(".content-main");
 message.addEventListener("change", (e) => {
   if (message.value.length != 0) {
     icons.classList.add("none");
@@ -16,9 +17,30 @@ message.addEventListener("change", (e) => {
 
 // kirim pesan
 
+function startBot(x) {
+  return `  <div class="left">
+  <div class="card-pesan">
+  <p>${x}</p>
+  </div>
+  </div>`;
+}
+function startUser(x) {
+  return `<div class="right">
+  <div class="card-pesan">
+  <p>${x}</p>
+  </div>
+  </div>`;
+}
+
 btnSend.addEventListener("click", (e) => {
-  const x = `<div class="card-pesan right">
-    <p>${message.value}</p>
-        </div>`;
-    contentRight.innerHTML += x;
+  contentMain.innerHTML += startUser(message.value);
+  window.scrollTo(0, contentMain.scrollHeight);
+  setTimeout(() => {
+    if(message.value == "hello" || "hi" || "hai"){
+      let chatBot = `${message.value} ada yang bisa saya bantu?`
+      contentMain.innerHTML +=  startBot(chatBot);
+      // startBot(chatBot);
+      window.scrollTo(0, contentMain.scrollHeight);
+    }
+  },1000)
 });
